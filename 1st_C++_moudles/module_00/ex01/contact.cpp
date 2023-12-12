3d4b06a999f695e037c13bfad1a43d0d7236c481
Ahsan Abdul Salam <ahsanbaloch899@gmail.com>
2023-12-12 17:42:13 +0100

    added

---
+++ 1st_C++_moudles/module_00/ex01/contact.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contact.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahsalam <ahsalam@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 13:36:17 by ahsalam           #+#    #+#             */
+/*   Updated: 2023/12/12 16:28:56 by ahsalam          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "contact.hpp"
+
+Contact::Contact()	{}
+
+std::string Contact::Get_FName() const
+{
+    return _First_Name;
+}
+
+void    Contact::Set_FName(std::string &fname) 
+{
+    _First_Name = fname;
+}
+
+std::string Contact::Get_LName() const
+{
+    return _Last_Name;
+}
+
+void    Contact::Set_LName(std::string &lname) 
+{
+    _Last_Name = lname;
+}
+
+std::string Contact::Get_Number() const
+{
+    return _Number;
+}
+
+void    Contact::Set_Number(std::string &number) 
+{
+    _Number = number;
+}
+
+std::string Contact::Get_NName() const
+{
+    return _Nick_Name;
+}
+
+void    Contact::Set_NName(std::string &nick_name) 
+{
+    _Number = nick_name;
+}
+
+std::string Contact::Get_DSecret() const
+{
+    return _Darkest_secret;
+}
+
+void    Contact::Set_DSecret(std::string &dark_secret) 
+{
+    _Darkest_secret = dark_secret;
+}
+
+Contact::~Contact()	{}
--- 1st_C++_moudles/module_00/ex01/main.cpp
+++ 1st_C++_moudles/module_00/ex01/main.cpp
@@ -6,27 +6,28 @@
 /*   By: ahsalam <ahsalam@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 23:48:19 by ahsalam           #+#    #+#             */
-/*   Updated: 2023/12/10 23:59:28 by ahsalam          ###   ########.fr       */
+/*   Updated: 2023/12/11 14:24:39 by ahsalam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "phonebook.hpp"
+#include "contact.hpp"
 
 int main()
 {
 	std::string input;
+	Contact 	contact;
 
 	while (1)
 	{
 		std::cout << "Choose between (add, search, exit)" << std::endl;
 		std::cin >> input;
 		if (input == "add")
-			std::cout << "adding" << std::endl;
+			contact.Add();
 		else if (input == "search")
-			std::cout << "searching" << std::endl;
+			contact.Search();
 		else if (input == "exit")
 			break;
-		else 
+		else
 			std::cout << "Please choose from above given choices" << std::endl;
 	}
 	return (0);
