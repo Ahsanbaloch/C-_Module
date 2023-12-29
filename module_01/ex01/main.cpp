a7a59722e9dca710a70affb0177c6f9a352d2dce
Ahsan Abdul Salam <39459572+Ahsanbaloch@users.noreply.github.com>
2023-12-29 14:33:45 +0000

    added

--- module_01/ex01/main.cpp
+++ module_01/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahsalam <ahsalam@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ahsalam <ahsalam@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 21:02:02 by ahsalam           #+#    #+#             */
-/*   Updated: 2023/12/24 21:10:46 by ahsalam          ###   ########.fr       */
+/*   Updated: 2023/12/29 11:16:48 by ahsalam          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int main()
     delete[] zombie;
 
 /*     i = -1;
-    Zombie *zombie2 = zombieHorder(9, "Porsche 911 Gt3 RS  > Otehr_Porsche");
-     while(++i < 9)
+    Zombie *zombie2 = zombieHorder(11, "Porsche 911 Gt3 RS  > Otehr_Porsche");
+     while(++i < 11)
         zombie[i].announce();
     delete[] zombie2; */
     return (0);
